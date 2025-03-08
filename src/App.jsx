a3e0e1b6cdc93ebcf23a79a1693892aca930d439
Khwaja Abdul Samad <thatsme104@gmail.com>
2025-03-08 19:48:28 +0530

    added home section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./index.css"
 import { LoadingScreen } from './components/LoadingScreen'
 import { Navbar } from './components/Navbar'
 import { MobileMenu } from './components/MobileMenu'
+import { Home } from './components/sections/Home'
+import { About } from './components/sections/About'
 import { useState } from 'react'
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
         <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
         <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        <Home />
+        <About />
       </div >
 
     </>
---
+++ src/components/sections/About.jsx
@@ -0,0 +1,7 @@
+export const About = () => {
+  return <section
+    id="about"
+    className="min-h-screen flex ">
+
+  </section>
+}
