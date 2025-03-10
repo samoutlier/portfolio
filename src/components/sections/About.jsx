837c21b5bec740efefbdcd9b39b3ecccd73c1460
Khwaja Abdul Samad <thatsme104@gmail.com>
2025-03-10 23:57:09 +0530

    Updated about section with education languages headers etc

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,4 +1,9 @@
 export const About = () => {
+
+  const frontendSkills = ["HTML", "Bootstrap", "Tailwind", "JavaScript", "React",]
+
+  const languages = ["C", "C++", "Java", "JavaScript",]
+
   return <section
     id="about"
     className="min-h-screen flex items-center justify-center py-20"
@@ -17,11 +22,34 @@ export const About = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-          <h3>Frontend</h3>
+          <h3 className="text-xl font-bold mb-4">Languages</h3>
           <div className="flex flex-wrap gap-2">
-            
+            {languages.map((skill, key) => (
+              <span key={key} className="bg-blue-500/10 text-blue-500 py-2 px-5 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">{skill} </span>
+            ))}
           </div>
         </div>
+        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+          <h3 className="text-xl font-bold mb-4">Frontend</h3>
+          <div className="flex flex-wrap gap-2">
+            {frontendSkills.map((skill, key) => (
+              <span key={key} className="bg-blue-500/10 text-blue-500 py-2 px-5 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">{skill} </span>
+            ))}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">🎓  Education</h3>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">📟 Coding Profiles</h3>
+          </div>
+        </div>
+
       </div>
 
     </div>
