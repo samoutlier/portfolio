3ce676d4ae69463d106fa7afc82fcdadeea70436
Khwaja Abdul Samad <thatsme104@gmail.com>
2025-03-12 18:21:07 +0530

    Added Scroll Component to enhance scroll effect through various sections

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,5 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
 export const About = () => {
 
   const frontendSkills = ["HTML", "Bootstrap", "Tailwind", "JavaScript", "React",]
@@ -8,89 +10,92 @@ export const About = () => {
     id="about"
     className="min-h-screen flex items-center justify-center py-20"
   >
-    <div className="max-w-3xl mx-auto px-4 ">
+    <RevealOnScroll>
 
-      <h2 className="text-4xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent ">
-        About Me
-      </h2>
+      <div className="max-w-3xl mx-auto px-4 ">
 
-      <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all ">
-        <p className="glass text-gray-300 mb-6">
-          Passionate developer with expertise in building scalable web applications and creating innovative solutions.
-        </p>
-      </div>
+        <h2 className="text-4xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent ">
+          About Me
+        </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-          <h3 className="text-xl font-bold mb-4">Languages</h3>
-          <div className="flex flex-wrap gap-2">
-            {languages.map((skill, key) => (
-              <span key={key} className="bg-blue-500/10 text-blue-500 py-2 px-5 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">{skill} </span>
-            ))}
-          </div>
-        </div>
-        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-          <h3 className="text-xl font-bold mb-4">Frontend</h3>
-          <div className="flex flex-wrap gap-2">
-            {frontendSkills.map((skill, key) => (
-              <span key={key} className="bg-blue-500/10 text-blue-500 py-2 px-5 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">{skill} </span>
-            ))}
-          </div>
+        <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all ">
+          <p className="glass text-gray-300 mb-6">
+            Passionate developer with expertise in building scalable web applications and creating innovative solutions.
+          </p>
         </div>
 
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">Languages</h3>
+            <div className="flex flex-wrap gap-2">
+              {languages.map((skill, key) => (
+                <span key={key} className="bg-blue-500/10 text-blue-500 py-2 px-5 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">{skill} </span>
+              ))}
+            </div>
+          </div>
+          <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">Frontend</h3>
+            <div className="flex flex-wrap gap-2">
+              {frontendSkills.map((skill, key) => (
+                <span key={key} className="bg-blue-500/10 text-blue-500 py-2 px-5 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">{skill} </span>
+              ))}
+            </div>
+          </div>
 
-        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all">
-          <h3 className="text-xl font-bold mb-4">🎓  Education</h3>
-          <ul className="list-disc list-inside text-gray-300 space-y-4">
-            <li>
-              <strong>
-                B.Tech in Electronics & Communication Engineering
-              </strong> - Jamia Millia Islamia, New Delhi (2020-2024)
+
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">🎓  Education</h3>
+            <ul className="list-disc list-inside text-gray-300 space-y-4">
+              <li>
+                <strong>
+                  B.Tech in Electronics & Communication Engineering
+                </strong> - Jamia Millia Islamia, New Delhi (2020-2024)
+              </li>
+              <li>
+                Relevant Coursework : Data Structures & Algorithms, OOPS, Computer Networks, DBMS
+              </li>
+            </ul>
+          </div>
+
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">📟 Coding Profiles</h3>
+            <li className="text-xl">
+              <a
+                href="https://www.leetcode.com/"
+                className="text-blue-400 hover:text-blue-300 transition-colors"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LeetCode
+              </a>
             </li>
             <li>
-              Relevant Coursework : Data Structures & Algorithms, OOPS, Computer Networks, DBMS
+              <a
+                href="https://www.geeksforgeeks.com/"
+                className="text-xl text-blue-400 hover:text-blue-300 transition-colors"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GeeksForGeeks
+              </a>
             </li>
-          </ul>
-        </div>
+            <li>
+              <a
+                href="https://www.codechef.com/"
+                className="text-xl text-blue-400 hover:text-blue-300 transition-colors"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                CodeChef
+              </a>
+            </li>
+          </div>
 
-        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all">
-          <h3 className="text-xl font-bold mb-4">📟 Coding Profiles</h3>
-          <li className="text-xl">
-            <a
-              href="https://www.leetcode.com/"
-              className="text-blue-400 hover:text-blue-300 transition-colors"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LeetCode
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.geeksforgeeks.com/"
-              className="text-xl text-blue-400 hover:text-blue-300 transition-colors"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GeeksForGeeks
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.codechef.com/"
-              className="text-xl text-blue-400 hover:text-blue-300 transition-colors"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CodeChef
-            </a>
-          </li>
-        </div>
 
+        </div>
 
       </div>
 
-    </div>
-
+    </RevealOnScroll>
   </section>
 }
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,9 +1,13 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
 export const Home = () => {
   return (
     <section
       id="home"
       className="min-h-screen flex items-center justify-center relative"
     >
+      <RevealOnScroll>
+
       <div className="text-center z-10 px-4">
         <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent"> Hi, I'm Khwaja Abdul Samad
         </h1>
@@ -20,6 +24,7 @@ export const Home = () => {
         </div>
       </div>
 
+      </RevealOnScroll>
     </section>
   );
 }
